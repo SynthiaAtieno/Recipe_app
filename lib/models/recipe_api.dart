3d646e6393bdccf5ccdd01aafeec7ee399c1e346
SynthiaAtieno <ondengsynthia222@gmail.com>
2023-12-11 15:16:54 +0300

    latest commit

---
+++ lib/models/recipe_api.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:simple_recipe/models/recipe.dart';
+
+class RecipeApi{
+
+  static Future<List<Recipe>> getRecipe() async{
+    var uri = Uri.parse('https://yummly2.p.rapidapi.com/feeds/list');
+    final response = await http.get(uri, headers: {
+      'X-RapidAPI-Key': '74d8874198msh211246385474078p14348fjsn097110f3f554',
+      'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+    });
+
+    Map data = jsonDecode(response.body);
+    List temp = [];
+
+    for(var i in data['feed']){
+      temp.add(i['content']['details']);
+    }
+    return Recipe.recipeFromSnapshot(temp);
+}
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:simple_recipe/models/recipe_api.dart';
+import 'package:simple_recipe/views/widgets/reciep_card.dart';
+
+import '../models/recipe.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Recipe> _recipe;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+
+  }
+  Future<void> getRecipes() async{
+    _recipe = await RecipeApi.getRecipe();
+    setState(() {
+      isLoading = false;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.restaurant_menu_outlined),
+             SizedBox(width: 10,),
+            Text("Food Recipe")
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: const [
+            RecipeCard(title: "title", cookTime: "cookTime", rating: "rating", thumbnailUrl: "https://lh3.googleusercontent.com/ecJftfvxSWg45EZKGT0iMOc5Lc0yYxZiqRnTriRxyn4iIgnX-sARTNUXrTUKSBg4j3JZYLhYLq84ZDXGZXsAQQ=s360")
+          ],
+        ),
+      ),
+    );
+  }
+}
